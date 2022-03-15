c5af91999bd2f6571ddcafd37a025d74856da20d
dhofmann34 <dennis@planethofmann.com>
2022-03-15 11:23:50 -0400

    connect to DB

--- finalapp.py
+++ finalapp.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask import request
 from flask import Flask, request, render_template, flash, redirect, url_for, Response, send_file
 #from werkzeug.utils import secure_filename
 import os
@@ -9,7 +9,8 @@ from shutil import copyfile
 import psycopg2
 from flask import jsonify
 from flask_navigation import Navigation
-# from config import config
+from config import config
+
 
 
 #### Set up nav menue ####
@@ -26,13 +27,60 @@ nav.Bar('top', [
 ])
 
 #### Home page ####
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template('index.html')
 
 @app.route('/profile', methods=['GET'])
 def profile():
     return render_template('profile.html')
+
+
+#### Pull data from front end | store data in DB | send data back to frontend ####
+@app.route('/data', methods=['POST', 'GET'])
+def temp():
+    data = request.get_data().decode("utf-8")
+    if(data != ''):
+        store_db(data)
+    return data
+
+# take user input and store in database
+def store_db(data):
+    conn = None
+    try:
+        # read connection parameters
+        params = config()  # get DB info from config.py
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        cur = conn.cursor()  # create a cursor
+        cur.execute(f"INSERT INTO posts(post) VALUES ('{data}')")  # insert into DB
+        conn.commit()
+        conn.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed, inserted successfully.')
+    
+@app.route('/data_front')
+def send_data():
+    params = config()  # get DB info from config.py
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()  # create a cursor
+    cur.execute("SELECT * FROM POSTS")  # query DB 
+    field_names = [i[0] for i in cur.description]
+    result =  result = [dict(zip(field_names,row)) for row in cur.fetchall()]
+    conn.close()
+    return jsonify(result)
+
+
+
 
 
 if __name__ == '__main__':
--- static/javascript/javascript.js
+++ static/javascript/javascript.js
@@ -1,1 +1,29 @@
-console.log("hi");
+$("form").on("submit", save_user_input)  // On submit start process
+
+// Takes user input and sends it to backend
+function save_user_input(event) {
+    input = $("input").val();  // user input
+    console.log(input);
+    // $.ajax({
+    //     type:"POST",
+    //     url: "/data",
+    //     data: JSON.stringify(input)
+    // });
+
+    const request = new XMLHttpRequest();  // POST
+        request.open("POST", "/data")
+        data = input
+        request.send(data)
+
+
+}
+
+// Get data from backend and store in table
+document.onload = $.getJSON('./data_front', function(data){
+    let text = "<table border='1'>"
+    for (let x in data) {
+      text += "<tr><td>" + Object.values(data[x]) + "</td></tr>";
+    }
+    text += "</table>"    
+    document.getElementById("demo").innerHTML = text;
+  });
