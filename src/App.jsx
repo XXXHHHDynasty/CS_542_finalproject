fd1b1cab76de7d412afed01c28c43c624ad9e410
XXXHHHDynasty <421735132@qq.com>
2022-04-12 19:41:06 -0400

    ✨ feat: server titles display according to json

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Navigate, useNavigate, Outlet } from 'react-router-dom'
 import { Layout, Menu } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
@@ -7,8 +7,49 @@ import 'antd/dist/antd.min.css'
 const { Header, Sider } = Layout;
 const { SubMenu } = Menu;
 
-
 const App = () => {
+    const [menuData, setMenuData] = useState();
+
+    const axios = require('axios').default;
+    const getData = async () => {
+        const res = await axios(
+            'http://localhost:3000/servers',
+            {
+                method: 'get',
+            },
+        );
+        return res;
+    }
+
+    const testres = () => {
+        getData()
+    }
+
+    const getMenu = (data) => {
+        return Object.values(data).map((item, index) => {
+            return (
+                <SubMenu key={item.id} title={item.title} icon={<UserOutlined />}>
+                    {getSubMenu(item.subServers)}
+                </SubMenu>
+            )
+        })
+    }
+
+    const getSubMenu = (data) => {
+        return Object.values(data).map((item, index) => {
+            return (
+                <Menu.Item key={item.id}>{item.title}</Menu.Item>
+            )
+        })
+    }
+
+    useEffect(() => {
+        getData().then(res => {
+            const data_menu = getMenu(res.data);
+            setMenuData(data_menu);
+        });
+    }, []);
+
     const navigate = useNavigate()
 
     const goHome = () => {
@@ -48,34 +89,7 @@ const App = () => {
                     defaultOpenKeys={['sub1']}
                     style={{ height: '100%', borderRight: 0 }}
                 >
-                    <SubMenu key="sub1" icon={<UserOutlined />} title="Server">
-                        <Menu.Item key="1" onClick={goHome}>Subserver 1</Menu.Item>
-                        <Menu.Item key="2">Subserver 2</Menu.Item>
-                        <Menu.Item key="3">Subserver 3</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" icon={<UserOutlined />} title="WPI Lifestyle">
-                        <Menu.Item key="4">Free chatting</Menu.Item>
-                        <Menu.Item key="5">Foods</Menu.Item>
-                        <Menu.Item key="6">Games</Menu.Item>
-                        <Menu.Item key="7">Sports</Menu.Item>
-                        <Menu.Item key="8">Musics</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub3" icon={<UserOutlined />} title="WPI Study">
-                        <Menu.Item key="9">Welcome</Menu.Item>
-                        <Menu.Item key="10">Courses</Menu.Item>
-                        <Menu.Item key="11">Project help</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub4" icon={<UserOutlined />} title="Fun things sharing place">
-                        <Menu.Item key="12">Free chatting</Menu.Item>
-                        <Menu.Item key="13">Memes</Menu.Item>
-                        <Menu.Item key="14">Pictures</Menu.Item>
-                        <Menu.Item key="15">Videos</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub5" icon={<UserOutlined />} title="Only for complaints">
-                        <Menu.Item key="16">Free complaining</Menu.Item>
-                        <Menu.Item key="17">Gently complain</Menu.Item>
-                        <Menu.Item key="18">Hell level complain</Menu.Item>
-                    </SubMenu>
+                    {menuData}
                 </Menu>
             </Sider>
             <Outlet />
