6813880fb4eb2f20715989077894f7714a22adac
TonyC793 <hchen9@wpi.edu>
2022-04-13 21:13:27 -0400

    update login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,62 @@
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { Button, Checkbox, Form, Input, Select, message } from 'antd';
 import '/node_modules/antd/dist/antd.css';
 
 import illustration from '../../images/WPIlogo.jpeg';
+
 import passwordImg from '../../images/password.png';
 import usernameImg from '../../images/username.png';
 
 import './login.css';
 
+const axios = require('axios').default;
+//login api function
+const login = (request) => {
+  return axios({
+      method: 'post',
+      url: 'http://localhost:3000/login',
+      data: {
+          "username": request.username,
+          "password": request.password
+      }
+  })
+}
+
 const Login = () => {
   const navigate = useNavigate()
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  
+  function usernameChange(e){
+    setUsername(e.target.value);
+  }
+
+  function passwordChange(e){
+    setPassword(e.target.value);
+  }
+
+  async function loginClick(){
+    let data = await login({
+      "username" : username,
+      "password" : password
+    });
+    if(data.status == 200){
+      navigate("/home");
+    }else{
+      console.log("login fail")
+    }
+  }
 
   const goSignup = () => {
     navigate('/signup', {})
   }
 
-  const goHome = () => {
-    navigate('/home', {
-      state: { username: "testUsername" }
-    })
-  }
+  // const goHome = () => {
+  //   navigate('/home', {
+  //     state: { username: "testUsername" }
+  //   })
+  // }
 
   const [form] = Form.useForm();
 
@@ -61,10 +97,11 @@ const Login = () => {
             <Form.Item
               label="Username"
               name="username"
+              onChange={usernameChange}
               rules={[
                 {
                   required: true,
-                  message: 'Please input your username!',
+                  message: 'Please input your username',
                 },
               ]}
             >
@@ -73,10 +110,11 @@ const Login = () => {
             <Form.Item
               label="Password"
               name="password"
+              onChange={passwordChange}
               rules={[
                 {
                   required: true,
-                  message: 'Please input your password!',
+                  message: 'Please input your password',
                 },
               ]}
             >
@@ -100,7 +138,7 @@ const Login = () => {
                 span: 16,
               }}
             >
-              <Button onClick={goHome} type="primary" htmlType="submit">
+              <Button onClick={loginClick} type="primary" htmlType="submit">
                 Log In
               </Button>
             </Form.Item>
@@ -116,7 +154,7 @@ const Login = () => {
             </Button>
             <Button
               type="link"
-              onClick={goHome}
+              
             >
               Enter as anonymous user
             </Button>
